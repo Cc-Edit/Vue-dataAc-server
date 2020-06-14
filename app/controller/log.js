856e98bd91d720d752d4c3fb20e9bf73fe3e9e81
wenqiang.li <wenqiang.li@renren-inc.com>
2020-06-14 19:26:24 +0800

    init

--- app/controller/log.js
+++ app/controller/log.js
@@ -7,15 +7,22 @@ class LogController extends Controller {
     const { ctx } = this;
     const { request = {} } = ctx;
     const { query = {} } = request;
-    const { uuid } = query;
+    const { uuid, type = '' } = query;
     if(!uuid){
       ctx.body = { isOk:false, msg:"用户数据不存在", data: [] };
       return;
     }
-    const { AcData } = ctx.model;
-    let result = await AcData.find({
+    let selOption = {
       uuid: uuid
-    }).limit(100)
+    };
+
+    if(type){
+      selOption.type = type;
+    }
+
+    const { AcData } = ctx.model;
+    let result = await AcData.find(selOption).limit(100);
+
     ctx.body = { isOk:true, msg:"获取成功" , data: result};
   }
 }
